9b6764c39880aa9a4b2449530d549fe58c564160
Siddhant Tripathy <siddhant.tripathy7@gmail.com>
2022-09-16 00:37:13 +0530

    changes made on documenting and formating of code

--- Code/Data_cleaning.R
+++ Code/Data_cleaning.R
@@ -1,11 +1,55 @@
-#Objectives : 
-#To clean the coal consumption data using R 
+# Data Wrangling in R
+# Coal Consumption Case Study
 
-#Importing the data:
+# Load the tidyverse
 library(tidyverse)
-coal <- read.csv("C:/Users/siddh/Desktop/Data_Analysis/Raw_Data/coal.csv", skip=2)
 
-#setting up the column name : 
- colnames(coal)[1] <- "Region"
-summary(coal) 
+# Read in the coal dataset
+coal <- read_csv("http://594442.youcanlearnit.net/coal.csv")
+glimpse(coal)
+
+# Skip the first two lines
+coal <- read_csv("http://594442.youcanlearnit.net/coal.csv", skip=2)
+glimpse (coal)
+
+# Rename the first column as region
+colnames(coal)[1] <- "region"
+summary(coal)
+
+# Convert from a wide dataset to a long dataset using pivot_longer
+coal_long <- pivot_longer(coal, !region, names_to='year', values_to='coal_consumption')
+glimpse(coal_long)
+
+# Convert years to integers
+coal_long <- coal_long %>%
+  mutate(year=as.integer(year))
+
+summary(coal_long)
+
+# Convert coal consumption to numeric
+coal_long <- coal_long %>%
+  mutate(coal_consumption=as.numeric(coal_consumption))
+
+summary(coal_long)
+
+# Look at region values - they contain both continents and countries
+unique(coal_long$region)
+
+# Create a vector of "noncountry" values that appear in the region variable
+noncountries <- c("North America", "Central & South America", "Antarctica", "Europe", "Eurasia", 
+                  "Middle East", "Africa", "Asia & Oceania", "World")
+
+# create a tibble of regional values
+coal_region <- coal_long %>% filter(region %in% noncountries)
+
+# create a tibble of country values
+coal_country <- coal_long %>% filter(!(region %in% noncountries))
+
+# check them out
+unique(coal_region$region)
+unique(coal_country$region)
+
+# Start with a scatterplot
+ggplot(data=coal_region, mapping=aes(x=year, y=coal_consumption)) +
+  geom_line(mapping=aes(color=region))
 
